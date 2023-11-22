9428e66a75258e7fba6b39e8e024c2de6ddf0dbb
CybXII <luft.luft86@googlemail.com>
2023-11-22 04:04:16 +0100

    -build functions to switch/close pic`s in full -optimized styles

--- Fotogallery Projekt Modul 7/script.js
+++ Fotogallery Projekt Modul 7/script.js
@@ -1,6 +1,7 @@
-let images = ['img/1.jpg','img/2.jpg','img/3.jpg','img/4.png','img/5.jpg','img/6.jpg','img/7.jpg','img/8.jpg','img/9.jpg','img/10.jpg','img/11.jpg']
+let images = ['img/1.jpg','img/2.jpg','img/3.jpg','img/4.png','img/5.jpg','img/6.jpg','img/7.jpg','img/8.jpg','img/9.jpg','img/10.jpg','img/11.jpg','img/12.jpg','img/13.jpg','img/14.jpg','img/15.jpg','img/16.jpg','img/17.jpg','img/18.jpg','img/19.jpg','img/20.jpg']
 
 function load() {
+    reset()
     for (let i = 0; i < images.length; i++) {
         document.getElementById('foto_board').innerHTML +=`
         <div onclick="openImage(${i})" class="imgcontainer" >
@@ -8,5 +9,51 @@ function load() {
         </div>
         `;
     }
+}
 
+function openImage(index){
+    document.getElementById('foto_board').innerHTML =`
+    <div class="full_screen_container" >
+        <div onclick="load()" class="close_button">
+            <img class="close_button" src="./img/x.png" onclick="load()"></img>
+        </div>
+        <div class="foto_container">
+            <div>
+                <img onclick="switchPicB(${index})" class="arrow_left" src="./img/arrow.png" ></img>
+            </div>
+            <div>
+                <img class="foto_full" src="${images[index]}" alt="Bild"></img>
+            </div>
+            <div>
+                <img onclick="switchPicF(${index})" class="arrow_right" src="./img/arrow.png" ></img>            
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+function reset(){
+    document.getElementById('foto_board').innerHTML =``;
+}
+
+function switchPicB(index){
+    let newPic= index-1;
+    if (newPic==-1){
+        newPic=images.length-1;
+        openImage(newPic);
+    }
+    else{
+        openImage(newPic);
+    }
+}
+
+function switchPicF(index){
+    let newPic= index+1;
+    if (newPic>=images.length){
+        newPic=0;
+        openImage(newPic);
+    }
+    else{
+        openImage(newPic);
+    } 
 }
