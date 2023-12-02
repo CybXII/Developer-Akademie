abcef18c78556243596acda9270fa2823e49d0a1
CybXII <luft.luft86@googlemail.com>
2023-12-02 17:45:48 +0100

    new projekt instgram clon

---
+++ Instagram Projekt Modul 7/script.js
@@ -0,0 +1,32 @@
+let posts = [
+    {
+        'author': 'Tagesschau',
+        'image': './img/img1.jpg',
+        'description': 'Text 1 2 3 ',
+        'location': ''
+    },
+    {
+        'author': 'Tagesschau',
+        'image': './img/img2.jpg',
+        'description': 'Text 3 2 1 ',
+        'location': ''
+    }
+]
+
+function show(params){
+    document.getElementById('postcontainer').innerHTML +='';
+
+
+    for (let i = 0; i < posts.length; i++) {
+        const post = posts[i];
+        document.getElementById('postcontainer').innerHTML +=`
+        <div>
+            <img src="${post['image']}">
+            <div>${post['author']} </div>
+            <div>${post['description']}</div>
+            <div>${post['location']}</div>
+        </div>
+        `
+        
+    }
+}
