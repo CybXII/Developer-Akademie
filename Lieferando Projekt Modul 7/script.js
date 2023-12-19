ad2f558b95416ce15468a03355ffff91efd7dcd1
CybXII <luft.luft86@googlemail.com>
2023-12-19 22:11:22 +0100

    -added json for restaurants -added images

---
+++ Lieferando Projekt Modul 7/script.js
@@ -0,0 +1,28 @@
+let restaurants = [
+    {
+        name: 'Istanbul Grill',
+        menüs:['Pommes','Döner','Dönerteller'],
+        prices:[3.49, 7.49, 9.99],
+        imgs:['istanbulpommes.jpg','istanbuldoener.jpg','dönerteller.jpg']
+    },
+    {
+        name: 'Angelos Pizza',
+        menüs:['Salami Pizza', 'Hawaii Pizza', 'Pizza Speciale'],
+        prices:[6.99, 7.99, 7.49],
+        imgs:['salami-pizza.jpg','hawaii-pizza.jpg','speziale-pizza.jpg']
+    },
+    {
+        name: 'Ibo`s Döner',
+        menüs:['Pommes','Döner','Dönerteller'],
+        prices:[3.49, 6.99, 9.49],
+        imgs:['ibopommes.jpg','ibodoener.jpg','dönerteller.jpg']
+    },
+    {
+        name: 'Mythos',
+        menüs:['Bauernsalat','Kreta-Platte','Gyros'],
+        prices:[9.50, 18.50, 14.50],
+        imgs:['bauern-salad.jpg','kretaplatte.jpg','gyros.jpg']
+    },
+]
+
+
