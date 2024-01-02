1089f0c752131050e967e715be624f34b156c7b4
CybXII <luft.luft86@googlemail.com>
2024-01-02 21:52:52 +0100

    -optmized code with chatGPT

--- 1.Frontend Module/Modul-9/Pokedex/script.js
+++ 1.Frontend Module/Modul-9/Pokedex/script.js
@@ -22,87 +22,86 @@ async function loadAPI(){
     let url = `https://pokeapi.co/api/v2/pokemon?offset=0&limit=1400`;
     let response = await fetch(url);
     responseAsJson = await response.json();
-    responseLength = responseAsJson['count'];
-    init();
+    responseLength = responseAsJson['count']+1;
+    await init();
+    setTimeout((x) => {
+        renderPokemon(renderedPokemonNumber);
+}, 3000);
 }
 
-async function init(){
+
+//Mein Code 
+/*async function init(){
     for (let i = renderedPokemonNumber; i < responseLength; i++) {
         await loadPokemon(responseAsJson['results'][i-1],i-1);
         renderPokemon(loadedPokemonNumber)
         fetchedPokemons++
     }
-}
+}*/
 
-
-async function loadPokemon(index,i){
+//Mein Code 
+/*async function loadPokemon(index,i){
     let pokemonUrl = await fetch(index.url);
     let pokemonAsJson = await pokemonUrl.json();
     let speciesFetch = await fetch(pokemonAsJson.species.url);
     let speciesAsJson = await speciesFetch.json();
-    pokemonsUrl.push(pokemonUrl);
-    pokemonSpecies.push(speciesAsJson);
     let names = speciesAsJson['names'][5]['name'];
     fetchPokemons.name.push(names);
+}*/
+
+//Mein Code von ChatGPT Optimiert
+async function init(){
+    const fetchPromises = [];
+
+    for (let i = renderedPokemonNumber; i < responseLength; i++) {
+        fetchPromises.push(loadPokemon(responseAsJson['results'][i-1], i-1));
+    }
+
+    await Promise.all(fetchPromises);
+}
+
+//Mein Code von ChatGPT Optimiert
+async function loadPokemon(index, i){
+    const pokemonUrl = await fetch(index.url);
+    const pokemonAsJson = await pokemonUrl.json();
+    const speciesFetch = await fetch(pokemonAsJson.species.url);
+    const speciesAsJson = await speciesFetch.json();
+    const names = speciesAsJson['names'][5]['name'];
+    const imgs = await  pokemonAsJson.sprites.other["official-artwork"].front_default;
+    const types = await  pokemonAsJson.types;
+    fetchPokemons.name.push(names);
     fetchPokemons.id.push(i+1);
-/*    let imgs = pokemonAsJson['sprites']['other']['official-artwork']['front_default'];
-    let types = pokemonAsJson['types'];
     fetchPokemons.img.push(imgs);
     fetchPokemons.type.push(types);
-*/
 }
 
 
-async function testFetch(pokemonUrl){
-    let responseUrl = await fetch(pokemonUrl);
-    return responseUrl
-}
-
-
-async function loadSpecies(index){
-    let PokemonSpeciesName = index.species.url;
-    pokemonSpecies.push(PokemonSpeciesName[5]['name'])
-}
-
-
-function renderScreenInfos(){   
-    renderPokemon();
-}
-
-
-async function loadMore(){
- maxRender= maxRender+25;
- console.log(loadedPokemonNumber+25)
- console.log(renderedPokemonNumber+25)
- renderPokemon(loadedPokemonNumber);
+function loadMore(){
+        maxRender= maxRender+25;
+        console.log(loadedPokemonNumber+25)
+        console.log(renderedPokemonNumber+25)
+        renderPokemon(renderedPokemonNumber);
 }
 
 
 function renderPokemon(input){
-    if (fetchedPokemons>loadedPokemonNumber+50 && maxRender+50){
-        for (let i = input; loadedPokemonNumber < maxRender; i++) {
-            renderCard(i);
-            loadedPokemonNumber++ 
-        }
-    }else {
-        setTimeout((x) => {
-            renderPokemon(input)
-        }, 5000);
-     }
+    for (let i = input; i < maxRender; i++ & renderedPokemonNumber++) {
+        renderCard(i);
+        renderType(i);
+    }
+
 }
 
 
-async function fetchImg(index){
-    let pokemonUrl = await fetch(responseAsJson['results'][index-1].url);
-    let pokemonAsJson = await pokemonUrl.json();
-    let imgs = await  pokemonAsJson.sprites.other["official-artwork"].front_default;
-    let types = await pokemonAsJson['types'];
-    let img = document.getElementById(`img${index-1}`);
-    let typeLength = pokemonAsJson['types'].length
+function renderType(index){    
+    let pokemonIndex = fetchPokemons['id'].indexOf(index)
+    let img = document.getElementById(`img${pokemonIndex}`)
+    let imgs = fetchPokemons['img'][pokemonIndex];
+    let typeLength = fetchPokemons['type'][pokemonIndex].length;
     img.setAttribute(`src`,`${imgs}`)
     for (let j = 0; j < typeLength; j++) {
-        let pokeType =  types[j]['type']['name'];
-        document.getElementById(`type${index-1}`).innerHTML +=`
+        let pokeType =  fetchPokemons['type'][pokemonIndex][j]['type']['name'];
+        document.getElementById(`type${pokemonIndex}`).innerHTML +=`
         <p>${pokeType}</p>
         `;
     }
@@ -110,18 +109,16 @@ async function fetchImg(index){
 
 
 function renderCard(index){
+    let pokemonIndex = fetchPokemons['id'].indexOf(index)
     let card = document.getElementById('pokedex_screen');
-    let pokeName = fetchPokemons['name'][index-1];
-    let pokeImg = fetchPokemons['img'][index-1];
-    renderedPokemonNumber = renderedPokemonNumber+1;
+    let pokeName = fetchPokemons['name'][pokemonIndex];
     card.innerHTML += `
     <div class="card">
-        <img id="img${index-1}" src="" class="card-img-top" alt="${pokeName}">
+        <img id="img${pokemonIndex}" src="" class="card-img-top" alt="${pokeName}">
         <div class="card-body">
             <h2 class="card-text">${pokeName}</h2>
-            <div class="types" id="type${index-1}"></div>
+            <div class="types" id="type${pokemonIndex}"></div>
         </div>
     </div>
     `;
-    fetchImg(index);
 }
